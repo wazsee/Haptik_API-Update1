da1a434837217b424aea3175e3ca9015d5d958ef
Satish19 <92371111+wazsee@users.noreply.github.com>
2022-06-15 11:24:44 +0530

    Add files via upload

---
+++ Haptik_API-main (1)/Haptik_API-main/Milkrare/factory/connector1.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import os
+
+
+def milking(request_json):
+    try:
+        #df = pd.read_csv(r"C:\Users\satishkumar.s\Desktop\Milkrare\factory\Haptikdata.csv",encoding='latin-1')
+        df = pd.read_csv('Haptik_API-main/Milkrare/factory/Haptikdata.csv')
+        print("df")
+        
+
+        #request_json['Milk1_Dry2'] = 1 if request_json['Milk1_Dry2'] == "milk" else 2
+
+        response_values = df.loc[
+            (df['Phone_Number']==request_json['Phone_Number'])
+            ]
+        print(df.head())
+
+        
+        response = {}
+        #response['Farmer Name'] = response_values['Farmer Name'].values[0]
+        response['Region Name'] = response_values['Region Name'].values[0]
+        response['District Name'] = response_values['District Name'].values[0]
+        response['Pin Code'] = response_values['Pin Code'].values[0] 
+        #response['Plant Name'] = response_values['Plant Name'].values[0]
+        response['Plant Code'] =int(response_values['Plant Code'].values[0])
+        response['MCC Name'] = response_values['MCC Name'].values[0]
+        response['MCC Code'] = int(response_values['MCC Code'].values[0])
+        
+        
+
+        return response
+    except Exception as e:
+        return str(e)
+    
